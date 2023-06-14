81bfd8e8ec0dd6ef1bc90a8daf65d88836704878
maqiling <maqiling@shanda.com>
2023-06-14 15:28:30 +0800

    add page

---
+++ src/packages/detail/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// import image from './assets/images/banner_bg_1.png'
+import './assets/styles/styles.scss'
+import './assets/styles/test.css'
+
+import test from "../../utils/test";
+
+export default () => (
+  <div>
+    <h1 className="title">this is detail page1!!!</h1>
+    {/* <img src={image} /> */}
+    <div><button onClick={test}>test</button></div>
+  </div>
+)
